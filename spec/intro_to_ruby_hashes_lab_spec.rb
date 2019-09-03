dcba5e8ef54a764c95d920736414ed4dfa9393d5
Edward A Polanco <eduardpola18@hotmail.com>
2019-09-03 23:40:02 +0000

    Done.

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -4,24 +4,28 @@ require_relative '../intro_to_ruby_hashes_lab.rb'
 describe "working with hashes" do
 
   describe "instantiating" do
-    describe "new_hash" do
+    describe new_hash = {
+       do
       it "creates an empty hash and returns it" do
         expect(new_hash).to eq({})
+      }
       end
     end
 
 
-    describe "my_hash" do
+    describe my_hash = { do
       it "creates and returns a valid hash that contains key/value pairs of your choice" do
         expect(my_hash).to be_a(Hash)
         expect(my_hash.keys.count).to_not eq(0)
+      }
       end
     end
 
-    describe "pioneer" do
+    describe pioneer = { do
       it "creates and returns a hash named pioneer whose key is a symbol :name and whose value is a string, 'Grace Hopper'" do
         expect(pioneer).to be_a(Hash)
-        expect(pioneer[:name]).to eq("Grace Hopper")
+        expect(pioneer[:name]).to eq('Grace Hopper')
+      }
       end
     end
 
